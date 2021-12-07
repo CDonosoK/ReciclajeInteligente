02688b3c26f80bb8cdc9b74e2dfdee517d42c44d
CDonosoK <clemente.donosok@gmail.com>
2021-12-07 08:26:32 -0300

    Archivos Iniciales

---
+++ README.md
@@ -0,0 +1,3 @@
+Comandos necesarios:
+- En una terminal correr el comando: npx react-native start 
+- En otra terminal correr el comando: npx react-native run-android
---
+++ android/app/src/main/java/com/reciclajeinteligente/MainActivity.java
@@ -0,0 +1,15 @@
+package com.reciclajeinteligente;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ReciclajeInteligente";
+  }
+}
